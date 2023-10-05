5966a846c2d47eca0dad9da2529e20927ab50c9d
Benjamin Holton <ben@holtron.dev>
2023-10-05 15:15:20 -0400

    Removing anything related to NetBigInteger
    I removed the `NetBigInteger` class and anything that referenced it (read, nothing). Also removed the tests comparing it against BigInteger and the Performance Benchmarks.

--- Holtron.Net.Benchmarks/Program.cs
+++ Holtron.Net.Benchmarks/Program.cs
@@ -1,6 +1,4 @@
 using BenchmarkDotNet.Running;
-using Holtron.Net.Benchmarks.Components;
 using Holtron.Net.Benchmarks.Encryption;
 
 var encryptionSummary = BenchmarkRunner.Run<EncryptionBenchmarks>();
-var bigIntSummary = BenchmarkRunner.Run<BigIntBenchmarks>();
